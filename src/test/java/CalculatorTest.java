2642f072dd389e881634bdbf8985595a637782e4
Jerzy Ramos <gmjerzy@gmail.com>
2020-10-15 19:07:38 -0600

    Test push

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,123 +1,167 @@
-import org.junit.Test;
+package edu.byu.cs329.constantpropagation;
 
+import edu.byu.cs329.cfg.ControlFlowGraph;
+import edu.byu.cs329.rd.ReachingDefinitions.Definition;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.VariableDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.BooleanLiteral;
+import org.eclipse.jdt.core.dom.CharacterLiteral;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.NullLiteral;
+import org.eclipse.jdt.core.dom.NumberLiteral;
+import org.eclipse.jdt.core.dom.StringLiteral;
+import org.eclipse.jdt.core.dom.TypeLiteral;
 
-import static org.junit.Assert.*;
-public class CalculatorTest {
 
-    @Test
-    public void addTest(){
-        Calculator myCalculator = new Calculator();
+public class ConstantPropagator {
 
-        assertEquals(6 , myCalculator.add(2,4));
-        assertEquals(6 , myCalculator.add(3,3));
-        assertEquals(6 , myCalculator.add(0,6));
-        assertEquals(6 , myCalculator.add(7,-1));
-        assertEquals( -6, myCalculator.add(-3,-3));
+    ControlFlowGraph cfg;
+    ReachingDefinitions rd;
 
+    public ConstantPropagator(ControlFlowGraph cfg, ReachingDefinitions rd) {
+        this.cfg = cfg;
+        this.rd = rd;
     }
 
+    public boolean propagate() {
+        /* Replace any use of a variable that has a single reaching definition
+           that is a literal with the literal (or if there are multiple definitions
+           that reach the use but both are the same literal)
+        */
 
-    @Test
-    public void subtractTest(){
-        Calculator myCalculator = new Calculator();
+        Statement root = controlFlowGraph.getStart();
+        boolean changed = false;
 
-        assertEquals(-2 , myCalculator.subtract(2,4));
-        assertEquals(2 , myCalculator.subtract(4,2));
-        assertEquals(-4 , myCalculator.subtract(0,4));
-        assertEquals(99999 , myCalculator.subtract(100000,1));
+        /*
+        a=3;
+        a=3;
+        
+        b=a => b=3
+        */
+        // create a linked list and push root
+        Set<Statement> usedNodes = new HashSet<>();
+        LinkedList<Statement> nodes = new LinkedList<>();
+        nodes.add(root);
 
-    }
+        while (nodes.size() > 0) {
 
-    @Test
-    public void multiplyTest(){
-        Calculator myCalculator = new Calculator();
+            // remove first node from linked list
+            Statement node = linkedList.removeFirst();
+            usedNodes.add(node);
 
-        assertEquals(8 , myCalculator.multiply(2,4));
-        assertEquals(8 , myCalculator.multiply(4,2));
-        assertEquals(-4 , myCalculator.multiply(-1,4));
-        assertEquals(0 , myCalculator.multiply(0,4));
-        assertEquals(400 , myCalculator.multiply(100,4));
+            // push first node's children to linked list
+            succs = cfg.getSuccs(node);
+            for (Statement s : succs) {
+                if (!usedNodes.contains(s)) {
+                    nodes.add(s);
+                }
+            }
 
-    }
+            // get right hand side definitions
+            Expression rightHandSide = getRhs(node);
 
+            if (rightHandSide == null) {
+                continue;
+            }
 
-    @Test
-    public void divideTest(){
-        Calculator myCalculator = new Calculator();
+            Set<Definition> definitions = reachingDefinitions.getReachingDefinitions(rightHandSide);
 
-        assertEquals(0 , myCalculator.divide(2,4));
-        assertEquals(2 , myCalculator.divide(4,2));
-        assertEquals(50 , myCalculator.divide(100,2));
-        assertEquals(25 , myCalculator.divide(50,2));
-        assertEquals(1 , myCalculator.divide(3,2));
+            // check if definitions all contain same literal
+            Statement literal = null; // first literal
+            for (Definition d : definitions) {
+                Statement statement = d.statement;
+                Expression rhs = getRhs(statement);
+                if (rightHandSide == null) {
+                    continue;
+                }
 
-    }
+                // check if literal
+                boolean isLiteral = isLiteralExpression(rhs);
+                if (!isLiteral) {
+                    continue;
+                }
 
-    @Test
-    public void divideByZeroFailure(){
-        Calculator myCalculator = new Calculator();
+                // if literal...
+                if (literal == null) {
+                    // set literal
+                    literal = rhs;
+                } else {
+                    // check if literal is same
+                    if (literal != rhs) {
+                        continue;
+                    }
+                }
+            }
 
-        String failureMessage =  "/ by zero";
+            if (literal == null) {
+                continue;
+            }
 
-        try{
-            myCalculator.divide(5, 0);
-            fail();
-        }
-        catch (ArithmeticException e){
-            assertTrue(failureMessage.contains(e.getMessage()));
-        }
-
-
-    }
-
-    @Test
-    public void fibonacciTest(){
-        Calculator myCalculator = new Calculator();
-
-        assertEquals(0 , myCalculator.fibonacciNumberFinder(0));
-        assertEquals(1 , myCalculator.fibonacciNumberFinder(1));
-        assertEquals(1 , myCalculator.fibonacciNumberFinder(2));
-        assertEquals(2 , myCalculator.fibonacciNumberFinder(3));
-        assertEquals(3 , myCalculator.fibonacciNumberFinder(4));
-        assertEquals(5 , myCalculator.fibonacciNumberFinder(5));
-        assertEquals(8 , myCalculator.fibonacciNumberFinder(6));
-        assertEquals(13 , myCalculator.fibonacciNumberFinder(7));
-        assertEquals(21 , myCalculator.fibonacciNumberFinder(8));
-        assertEquals(317811 , myCalculator.fibonacciNumberFinder(28));
-
-    }
-
-    @Test
-    public void intToBinaryNumberTest(){
-        Calculator myCalculator = new Calculator();
-
-        assertEquals("0" , myCalculator.intToBinaryNumber(0));
-        assertEquals("1010" , myCalculator.intToBinaryNumber(10));
-        assertEquals("10000" , myCalculator.intToBinaryNumber(16));
-        assertEquals("10100" , myCalculator.intToBinaryNumber(20));
-        assertEquals("1111100111" , myCalculator.intToBinaryNumber(999));
-        assertEquals("1111111111" , myCalculator.intToBinaryNumber(1023));
-
-    }
-
-    @Test
-    public void createUniqueIDTest(){
-        Calculator myCalculator = new Calculator();
-        Set<String> uniqueIDBank = new HashSet<String>();
-
-        for(int i = 0; i < 10; i++){
-            uniqueIDBank.add(myCalculator.createUniqueID("CS204"));
-        }
-
-        assertEquals(10 , uniqueIDBank.size());
-
-        for(String targetString:uniqueIDBank){
-            assertTrue(targetString.contains("CS204"));
+            // if all definitions contain same literal, replace rhs with literal
+            changed = true;
+            setRhs(node, literal);
         }
 
+        return changed;
     }
 
+    private boolean isLiteralExpression(ASTNode exp) {
+        return (exp instanceof BooleanLiteral)
+                || (exp instanceof CharacterLiteral)
+                || (exp instanceof NullLiteral)
+                || (exp instanceof StringLiteral)
+                || (exp instanceof TypeLiteral)
+                || (exp instanceof NumberLiteral);
+    }
 
+    private Expression getRhs(Statement statement) {
+        if (!(statement instanceof VariableDeclarationStatement
+                || statement instanceof ExpressionStatement)) {
+            return null;
+        }
+        Expression rightHandSide;
+        if (statement instanceof VariableDeclarationStatement) {
+            rightHandSide = ((VariableDeclarationFragment)
+                    ((VariableDeclarationStatement) statement)
+                            .fragments().get(0))
+                    .getInitializer();
+        } else {
+            rightHandSide =  ((Assignment)
+                    ((ExpressionStatement) statement).getExpression()).getRightHandSide();
+        }
+        return rightHandSide;
+    }
+
+    private void setRhs(Statement statement, Expression value) {
+        Expression rightHandSide;
+        if (statement instanceof VariableDeclarationStatement) {
+            rightHandSide = ((VariableDeclarationFragment)
+                    ((VariableDeclarationStatement) statement)
+                            .fragments().get(0))
+                    .setInitializer(value);
+        } else {
+            ((Assignment)
+                    ((ExpressionStatement) statement).getExpression()).setRightHandSide(value);
+        }
+    }
 }
